3df0780fa3e896be75096174eb353e19eec1c904
GabbyG0421 <gregorio.gc@obmontessori.edu.ph>
2026-09-04 07:54:37 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,58 @@
+studname = 'Heather' #String
+studage = 16 #___
+stud_height = 162 #___
+stud_country = ["Korea", "US", "Japan"]#___
+stud_new = False #___
+stud_info = {
+    "color" :"powder blue",
+    "car_brand": "Tesla",
+    "shoe_size":"9",
+    "bff": "Miel" } #___
+stud_fruitfave = {
+    "Strawberry",
+    "Dragon Fruit"
+}
+days_of_week = (
+    "Monday",
+    "Tuesday",
+    "Wedsneday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+)
+
+print("Name:", studname)
+print("Age:", studage)
+print("Height:", stud_height)
+
+
+
+
+
+
+
+
+Class = "Grade 10" #Tuple
+Section = "Emerald" #Strings
+NStudents = 32 #integer
+
+print("Grade 10",)
+
+
+
+"Addition:", 12+1478
+"Subraction:", 12-1478
+"Multiplication:", 12*1478
+"Division:", 12**1478
+"Floating point Division:", 12/1478
+"Floor Division:", 12//1478
+"Modulo:", 12%1478
+
+print("Addition:", 12+1478)
+print("Subraction:", 12-1478)
+print("Multiplication:", 12*1478)
+print("Division:", 12**1478)
+print("Floating point Division:", 12/1478)
+print("Floor Division:", 12//1478)
+print("Modulo:", 12%1478)
